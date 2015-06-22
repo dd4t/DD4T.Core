6df8115896a743925a18804138e761eb141fcb32
sshibani <siawash.shibani@gmail.com>
2015-06-22 14:47:40 +0200

    add Autofac bootstrapper

---
+++ source/DD4T.Bootstrap.Autofac/Bootstrap.cs
@@ -0,0 +1,61 @@
+﻿using Autofac;
+using DD4T.ContentModel.Contracts.Caching;
+using DD4T.ContentModel.Contracts.Configuration;
+using DD4T.ContentModel.Contracts.Logging;
+using DD4T.ContentModel.Contracts.Providers;
+using DD4T.ContentModel.Contracts.Resolvers;
+using DD4T.ContentModel.Factories;
+using DD4T.Factories;
+using DD4T.Mvc.Html;
+using DD4T.Utils;
+using DD4T.Utils.Caching;
+using DD4T.Utils.Logging;
+using DD4T.Utils.Resolver;
+using System;
+using System.Linq;
+
+namespace DD4T.Bootstrap.Autofac
+{
+    public static class Bootstrap
+    {
+        public static void UseDD4T(this ContainerBuilder builder)
+        {
+            var provider = AppDomain.CurrentDomain.GetAssemblies().Where(ass => ass.FullName.StartsWith("DD4T.Providers")).FirstOrDefault();
+            var providerTypes = provider.GetTypes();
+            var pageprovider = providerTypes.Where(a => typeof(IPageProvider).IsAssignableFrom(a)).FirstOrDefault();
+            var cpProvider = providerTypes.Where(a => typeof(IComponentPresentationProvider).IsAssignableFrom(a)).FirstOrDefault();
+            var linkProvider = providerTypes.Where(a => typeof(ILinkProvider).IsAssignableFrom(a)).FirstOrDefault();
+            var binaryProvider = providerTypes.Where(a => typeof(IBinaryProvider).IsAssignableFrom(a)).FirstOrDefault();
+            var componentProvider = providerTypes.Where(a => typeof(IComponentProvider).IsAssignableFrom(a)).FirstOrDefault();
+            var facadeProvider = providerTypes.Where(a => typeof(IProvidersFacade).IsAssignableFrom(a)).FirstOrDefault();
+
+            builder.RegisterType<DD4TConfiguration>().As<IDD4TConfiguration>().SingleInstance().PreserveExistingDefaults();
+            builder.RegisterType<DefaultPublicationResolver>().As<IPublicationResolver>().SingleInstance().PreserveExistingDefaults();
+            builder.RegisterType<NullLogger>().As<ILogger>().SingleInstance().PreserveExistingDefaults();
+            builder.RegisterType<DefaultComponentPresentationRenderer>().As<IComponentPresentationRenderer>().SingleInstance().PreserveExistingDefaults();
+
+            builder.RegisterType<DefaultCacheAgent>().As<ICacheAgent>().PreserveExistingDefaults();
+
+            if (facadeProvider != null)
+                builder.RegisterType(facadeProvider).As<IProvidersFacade>().PreserveExistingDefaults();
+            if (pageprovider != null)
+                builder.RegisterType(pageprovider).As<IPageProvider>().PreserveExistingDefaults();
+            if (cpProvider != null)
+                builder.RegisterType(cpProvider).As<IComponentPresentationProvider>().PreserveExistingDefaults();
+            if (binaryProvider != null)
+                builder.RegisterType(binaryProvider).As<IBinaryProvider>().PreserveExistingDefaults();
+            if (linkProvider != null)
+                builder.RegisterType(linkProvider).As<ILinkProvider>().PreserveExistingDefaults();
+            if (componentProvider != null)
+                builder.RegisterType(componentProvider).As<IComponentProvider>().PreserveExistingDefaults();
+
+
+            builder.RegisterType<FactoriesFacade>().As<IFactoriesFacade>().PreserveExistingDefaults();
+            builder.RegisterType<PageFactory>().As<IPageFactory>().PreserveExistingDefaults();
+            builder.RegisterType<ComponentPresentationFactory>().As<IComponentPresentationFactory>().PreserveExistingDefaults();
+            builder.RegisterType<ComponentFactory>().As<IComponentFactory>().PreserveExistingDefaults();
+            builder.RegisterType<BinaryFactory>().As<IBinaryFactory>().PreserveExistingDefaults();
+            builder.RegisterType<LinkFactory>().As<ILinkFactory>().PreserveExistingDefaults();
+        }
+    }
+}
--- source/DD4T.Bootstrap.Ninject/Bootstrap.cs
+++ source/DD4T.Bootstrap.Ninject/Bootstrap.cs
@@ -21,7 +21,7 @@ namespace DD4T.Bootstrap.Ninject
 {
     public static class Bootstrap
     {
-        public static IKernel UseDD4T(this IKernel kernel)
+        public static void UseDD4T(this IKernel kernel)
         {
             var provider = AppDomain.CurrentDomain.GetAssemblies().Where(ass => ass.FullName.StartsWith("DD4T.Providers")).FirstOrDefault();
             var providerTypes = provider.GetTypes();
@@ -31,6 +31,20 @@ namespace DD4T.Bootstrap.Ninject
             var facadeProvider = providerTypes.Where(a => typeof(IProvidersFacade).IsAssignableFrom(a)).FirstOrDefault();
             var binaryProvider = providerTypes.Where(a => typeof(IBinaryProvider).IsAssignableFrom(a)).FirstOrDefault();
             var componentProvider = providerTypes.Where(a => typeof(IComponentProvider).IsAssignableFrom(a)).FirstOrDefault();
+
+
+            if (kernel.TryGet<IDD4TConfiguration>() == null)
+                kernel.Bind<IDD4TConfiguration>().To<DD4TConfiguration>().InSingletonScope();
+
+            if (kernel.TryGet<ILogger>() == null)
+                kernel.Bind<ILogger>().To<NullLogger>().InSingletonScope();
+
+            if (kernel.TryGet<IPublicationResolver>() == null)
+                kernel.Bind<IPublicationResolver>().To<DefaultPublicationResolver>().InSingletonScope();
+
+            if (kernel.TryGet<ICacheAgent>() == null)
+                kernel.Bind<ICacheAgent>().To<DefaultCacheAgent>();
+
 
             if (kernel.TryGet<IBinaryProvider>() == null)
                 kernel.Bind<IBinaryProvider>().To(binaryProvider);
@@ -51,23 +65,12 @@ namespace DD4T.Bootstrap.Ninject
                 kernel.Bind<IProvidersFacade>().To(facadeProvider);
 
 
-            if (kernel.TryGet<IDD4TConfiguration>() == null)
-                kernel.Bind<IDD4TConfiguration>().To<DD4TConfiguration>().InSingletonScope();
-
-            if (kernel.TryGet<ILogger>() == null)
-                kernel.Bind<ILogger>().To<NullLogger>().InSingletonScope();
-
-            if (kernel.TryGet<ICacheAgent>() == null)
-                kernel.Bind<ICacheAgent>().To<DefaultCacheAgent>();
-
-            if (kernel.TryGet<IPublicationResolver>() == null)
-                kernel.Bind<IPublicationResolver>().To<DefaultPublicationResolver>().InSingletonScope();
-
-            //Kernel.Bind<ITaxonomyProvider>().To<TridionTaxonomyProvider>().InSingletonScope();
             if (kernel.TryGet<IPageFactory>() == null)
                 kernel.Bind<IPageFactory>().To<PageFactory>();
+
             if (kernel.TryGet<IComponentPresentationFactory>() == null)
                 kernel.Bind<IComponentPresentationFactory>().To<ComponentPresentationFactory>();
+
             if (kernel.TryGet<ILinkFactory>() == null)
                 kernel.Bind<ILinkFactory>().To<LinkFactory>();
 
@@ -81,7 +84,6 @@ namespace DD4T.Bootstrap.Ninject
             if (kernel.TryGet<IComponentPresentationRenderer>() == null)
                 kernel.Bind<IComponentPresentationRenderer>().To<DefaultComponentPresentationRenderer>().InSingletonScope();
 
-            return kernel;
         }
 
     }
