cbc2a1c2eb9d2e14c2c5ed78e967650d7716b346
Quirijn Slings <quirijn.slings@trivident.com>
2015-08-27 14:13:55 +0200

    added jmslistener

---
+++ source/DD4T.Utils/Caching/JMSListener.cs
@@ -0,0 +1,74 @@
+﻿using Apache.NMS;
+using Apache.NMS.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DD4T.Utils.Caching
+{
+    public class JMSListener
+    {
+        public static void Start(string hostname, int port, string topic)
+        {
+            // Example connection strings:
+            //    activemq:tcp://activemqhost:61616
+            //    stomp:tcp://activemqhost:61613
+            //    ems:tcp://tibcohost:7222
+            //    msmq://localhost
+
+            Uri connecturi = new Uri(string.Format("activemq:tcp://{0}:{1}", hostname, port));
+
+            //Console.WriteLine("About to connect to " + connecturi);
+
+            // NOTE: ensure the nmsprovider-activemq.config file exists in the executable folder.
+            IConnectionFactory factory = new NMSConnectionFactory(connecturi);
+
+            using (IConnection connection = factory.CreateConnection())
+            using (ISession session = connection.CreateSession())
+            {
+                // Examples for getting a destination:
+                //
+                // Hard coded destinations:
+                //    IDestination destination = session.GetQueue("FOO.BAR");
+                //    Debug.Assert(destination is IQueue);
+                //    IDestination destination = session.GetTopic("FOO.BAR");
+                //    Debug.Assert(destination is ITopic);
+                //
+                // Embedded destination type in the name:
+                //    IDestination destination = SessionUtil.GetDestination(session, "queue://FOO.BAR");
+                //    Debug.Assert(destination is IQueue);
+                //    IDestination destination = SessionUtil.GetDestination(session, "topic://FOO.BAR");
+                //    Debug.Assert(destination is ITopic);
+                //
+                // Defaults to queue if type is not specified:
+                //    IDestination destination = SessionUtil.GetDestination(session, "FOO.BAR");
+                //    Debug.Assert(destination is IQueue);
+                //
+                // .NET 3.5 Supports Extension methods for a simplified syntax:
+                //    IDestination destination = session.GetDestination("queue://FOO.BAR");
+                //    Debug.Assert(destination is IQueue);
+                //    IDestination destination = session.GetDestination("topic://FOO.BAR");
+                //    Debug.Assert(destination is ITopic);
+                //IDestination destination = SessionUtil.GetDestination(session, "queue://FOO.BAR");
+
+                IDestination destination = session.GetTopic(topic);
+
+                //Console.WriteLine("Using destination: " + destination);
+
+                // Create a consumer and producer
+                using (IMessageConsumer consumer = session.CreateConsumer(destination))
+                {
+                    // Start the connection so that messages will be processed.
+                    connection.Start();
+                    consumer.Listener += new MessageListener(OnInvalidate);
+                }
+            }
+        }
+        protected static void OnInvalidate(IMessage receivedMsg)
+        {
+            var m = receivedMsg;
+        }
+    }
+}
