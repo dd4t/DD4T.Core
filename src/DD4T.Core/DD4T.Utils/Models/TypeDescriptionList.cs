b52d838fcaa50766ef6373e08b3d248ed15c9e4f
sshibani <siawash.shibani@trivident.com>
2016-10-18 20:11:51 +0200

    add typedescription list model

---
+++ src/DD4T.Core/DD4T.Utils/Models/TypeDescriptionList.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DD4T.Core.DD4T.Utils.Models
+{
+    public class TypeDescriptionList : List<TypeDescription>
+    {
+        public void Add(Type interfaceType, Type implementationType, LifeCycle lifeCycle = LifeCycle.SingleInstance)
+        {
+            this.Add(new TypeDescription()
+            {
+                Interface = interfaceType,
+                Implementation = implementationType,
+                LifeCycle = lifeCycle
+            });
+        }
+    }
+
+    public struct TypeDescription
+    {
+        public Type Interface { get; set; }
+        public Type Implementation { get; set; }
+        public LifeCycle LifeCycle { get; set; }
+    }
+
+    public enum LifeCycle
+    {
+        PerRequest,
+        SingleInstance,
+        PerLifeTime,
+        PerDependency
+    }
+}
